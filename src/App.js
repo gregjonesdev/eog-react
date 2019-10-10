c9102611791fc251589426d0389cf6d0d7f05a7d
Greg Jones <gmjones@gmail.com>
2019-10-10 00:45:02 -0500

    add dummy data and colors to chart

--- src/App.js
+++ src/App.js
@@ -2,14 +2,32 @@ import React from "react";
 import createStore from "./store";
 import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import Button from '@material-ui/core/Button';
 import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
 import Wrapper from "./components/Wrapper";
 import NowWhat from "./components/NowWhat";
 
-import Button from '@material-ui/core/Button';
+const data = [
+  {name: 'Mon', uv: 400, pv: 2400, amt: 2400},
+  {name: 'Tue', uv: 500, pv: 2000, amt: 2800},
+  {name: 'Wed', uv: 475, pv: 2200, amt: 2500}, ];
+
+const renderLineChart = (
+  <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+    <Line type="monotone" dataKey="uv" stroke="#347B98" />
+    <Line type="monotone" dataKey="pv" stroke="#66B032" />
+    <Line type="monotone" dataKey="amt" stroke="#B2D732" />
+    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+    <XAxis dataKey="name" />
+    <YAxis />
+    <Tooltip />
+  </LineChart>
+);
+
 
 const store = createStore();
 const theme = createMuiTheme({
@@ -37,7 +55,8 @@ const App = props => (
         <Header />
         <Button variant="contained" color="primary">
           Dang
-        </Button>  
+        </Button>
+        {renderLineChart}
         <NowWhat />
         <ToastContainer />
       </Wrapper>
