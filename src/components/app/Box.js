e0aab0b5e88bbb327eb141e919cbef723bb96ec6
Greg Jones <gmjones@gmail.com>
2019-10-13 03:11:56 -0500

    builds query succesfully

--- src/components/app/Box.js
+++ src/components/app/Box.js
@@ -1,20 +1,78 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Provider, client, useQuery } from "../../core/client";
+import LinearProgress from "@material-ui/core/LinearProgress";
+
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Grid from '@material-ui/core/Grid';
 
-import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles({
-  card: {
-    margin: "5%"
-  }
-});
+const buildQuery = (metricName) => {
+  return `
+  query {
+      getLastKnownMeasurement(
+        metricName: "${ metricName }"
+      ) {
+      metric
+      at
+      value
+      unit
+    }
+    }
+  `;
+}
+
+
+const getWeather = state => {
+  const { temperatureinFahrenheit, description, locationName } = state.weather;
+  return {
+    temperatureinFahrenheit,
+    description,
+    locationName
+  };
+};
 
 export default ({metric}) => {
+  return (
+    <Provider value={client}>
+      <Box metric={metric}/>
+    </Provider>
+  );
+};
 
-  const classes = useStyles();
+const Box = ({metric}) => {
+
+  const dispatch = useDispatch();
+  // const { temperatureinFahrenheit, description, locationName } = useSelector(
+  //   getWeather
+  // );
+
+  const query = buildQuery(metric)
+
+  const metricName = "tubingPressure"
+
+  const [result] = useQuery({
+    query
+  });
+  const { fetching, data, error } = result;
+  useEffect(
+    () => {
+      if (error) {
+        // dispatch({ type: actions.API_ERROR, error: error.message });
+        console.log('errrrrror')
+        return;
+      }
+      if (!data) return;
+      const { getLastKnownMeasurement } = data;
+      console.log(getLastKnownMeasurement)
+      // dispatch({ type: actions.WEATHER_DATA_RECEIVED, getWeatherForLocation });
+    },
+    [dispatch, data, error]
+  );
+
+  if (fetching) return <LinearProgress />;
+
 
   return (
       <Grid item>
