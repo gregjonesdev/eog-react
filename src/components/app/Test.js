44a019b4ee8baa69a4a14acca1987d88d36f3ff7
Greg Jones <gmjones@gmail.com>
2019-10-22 02:25:03 -0500

    a little progress

--- src/components/app/Test.js
+++ src/components/app/Test.js
@@ -1,23 +1,53 @@
 import React, { useEffect } from "react";
+import TestChild from "./TestChild";
 import { useDispatch, useSelector } from "react-redux";
-import { Provider, useQuery } from "../../core/client";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import { LAST_MEASUREMENT_RECEIVED, API_ERROR } from "../../store/actions/metrics"
-import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider, Query, Subscription } from 'react-apollo';
 import { useSubscription } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 import { WebSocketLink } from 'apollo-link-ws';
 import { ApolloLink } from 'apollo-link';
 import { split } from 'apollo-link';
 import ApolloClient from 'apollo-boost';
+import { Provider, client, useQuery } from "../../core/client";
 import { HttpLink, createHttpLink } from 'apollo-link-http';
 import { ReduxCache, apolloReducer } from 'apollo-cache-redux';
 import ReduxLink from 'apollo-link-redux';
 import { onError } from 'apollo-link-error';
 import { getMainDefinition } from 'apollo-utilities';
 import { SubscriptionClient } from 'subscriptions-transport-ws';
-import { ssrExchange, Client, defaultExchanges, Subscription, subscriptionExchange, dedupExchange, cacheExchange, fetchExchange } from 'urql';
+// import { ssrExchange, Client, defaultExchanges, Subscription, subscriptionExchange, dedupExchange, cacheExchange, fetchExchange } from 'urql';
+
+
 const URL = 'react.eogresources.com';
+
+const clientNew = new ApolloClient({
+  uri: `https://${URL}/graphql/`,
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://${URL}/graphql`,
+  options: {
+    reconnect: true
+  }
+});
+
+const httpLink = new HttpLink({
+  uri: `https://${URL}/graphql`
+});
+
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
 
 const MEASUREMENT_SUBSCRIPTION = gql`
   subscription {
@@ -30,22 +60,11 @@ const MEASUREMENT_SUBSCRIPTION = gql`
   }
 `;
 
-const newMessages = `
-  subscription MessageSub {
-    newMessages {
-      id
-      from
-      text
-    }
-  }
-`;
 
-
-const buildQuery = (metricName) => {
-  return `
+const buildQuery = gql`
   query {
       getLastKnownMeasurement(
-        metricName: "${ metricName }"
+        metricName: "tubingPressure"
       ) {
       metric
       at
@@ -54,4 +73,52 @@ const buildQuery = (metricName) => {
     }
     }
   `;
-}
+
+const newNums = gql`
+  subscription {
+    newNum
+  }
+`;
+
+const getNums = gql`
+  {
+    heartbeat
+  }
+`;
+
+
+let unsubscribe=null;
+
+export default () => (
+  <ApolloProvider client={clientNew}>
+  <Query query={buildQuery}>
+    {({ loading, data, subscribeToMore }) => {
+      if (loading) {
+        return null;
+      }
+      if (!unsubscribe) {
+        unsubscribe = subscribeToMore({
+          document: MEASUREMENT_SUBSCRIPTION,
+          updateQuery: (prev, { subscriptionData }) => {
+            if (!subscriptionData.data) return prev;
+            console.log(subscriptionData)
+            // const { newNum } = subscriptionData.data;
+            // return {
+            //   ...prev,
+            //   getNums: ["...prev.getNums", "newNum"]
+            // };
+          }
+        });
+      }
+
+      return <div></div>;
+    }}
+
+
+
+
+
+
+  </Query>
+  </ApolloProvider>
+);
